884ec9f7078e7bed7e7bac8560c5ea5d212a38fb
IMFIL <Fslat092@uottawa.ca>
2016-04-03 11:12:19 -0400

    Brief UI

--- src/app.js
+++ src/app.js
@@ -5,35 +5,69 @@
  */
 
 var UI = require('ui');
-function gps(){
-  function success(pos) {
-    console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
+var Vector2 = require('vector2');
+var bussroutes = ["1","2","3","4"];
+var ID = [];
+var LAT = "";
+var LONG = "";
+
+for(var i = 0; i < 4; i++) {
+  var title = bussroutes[i];
+  title = title.charAt(0).toUpperCase() + title.substring(1);
+    
+  ID.push({
+      title:title,
+    });
   }
+
+var main = new UI.Window();
+
+var text = new UI.Text({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 168),
+  text:'BusNow Loading Data! ...',
+  font:'DROID_SERIF_28_BOLD',
+  color:'yellow',
+  textOverflow:'wrap',
+  textAlign:'center',
+	backgroundColor:'black'
   
-  function error(err) {
-    console.log('location error (' + err.code + '): ' + err.message);
-  }
-  
-  /* ... */
-  
-  // Choose options about the data returned
-  var options = {
-    enableHighAccuracy: true,
-    maximumAge: 10000,
-    timeout: 10000
-  };
-  
-  // Request current position
-  return navigator.geolocation.getCurrentPosition(success, error, options);
-}
-//var gpsCoord = 'hello world';
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: gps(),
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
 });
 
+main.add(text);
 main.show();
+
+
+function success(pos) {
+  LAT = pos.coords.latitude;
+  LONG = pos.coords.longitude;
+}
+
+function error(err) {
+  if(err.code == err.PERMISSION_DENIED) {
+    console.log('Location access was denied by the user.');  
+  } else {
+    console.log('location error (' + err.code + '): ' + err.message);
+  }
+}
+
+var options = {
+  enableHighAccuracy: true,
+  maximumAge: 10000,
+  timeout: 10000
+};
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+ShowOptions();
+
+function ShowOptions(){
+  var menu = new UI.Menu({
+    sections: [ {
+      items:ID
+    }]});
+  main.hide();
+  menu.show();
+}
+
+
+
