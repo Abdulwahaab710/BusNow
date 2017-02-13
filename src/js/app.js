d7271553aeaba405adae09b27f9ee352d4d534ae
Abdulwahaab Ahmed <Abdulwahaab710@users.noreply.github.com>
2017-02-13 12:19:15 -0500

    YAY finally the app works agin :D

--- src/js/app.js
+++ src/js/app.js
@@ -13,20 +13,14 @@ var LAT = '45.423664';
 var Destinations = [];
 var BusNumbers = [];
 var Times = [];
-//var key = true;
 
 function WRAP (){
+  
   for(var i = 0; i < BusNumbers.length; i++) {
     var title ="#" + BusNumbers[i] + "  in " +Times[i] + " mins";
-  console.log(title);
   //title = title.charAt(0).toUpperCase() + title.substring(1);
-  
   var subtitle = Destinations[i];
-  console.log('test '+Destinations[i]);
-
   //subtitle = subtitle.charAt(0).toUpperCase() + subtitle.substring(1);
-
-    
   ID.push({
       title:title,
       subtitle:subtitle      
@@ -53,58 +47,38 @@ var text = new UI.Text({
 main.add(text);
 main.show();
 
-/*function ShowWrong(){
-  ajax({
-    url:"https://maps.googleapis.com/maps/api/geocode/json?latlng=" + LAT +"," +LONG+"&key=" + apiKey,
-    type:'json'
-    },
-      function(data){
-        var Info = data.results[1].formatted_address;
-        var partsOfStr = Info.split(',');
-        var LOC = partsOfStr[1].replace(/\s/g,'');
-        if(LOC !== "Ottawa"){
-          console.log("inside of here");
-          var INFOSCREEN = new UI.Window();
-
-          var text = new UI.Text({
-          position: new Vector2(0, 0),
-          size: new Vector2(144, 168),
-          text:'Not available outside of Ottawa',
-          font:'DROID_SERIF_28_BOLD',
-          color:'yellow',
-          textOverflow:'wrap',
-          textAlign:'center',
-	        backgroundColor:'black'
-  
-});
-
-INFOSCREEN.add(text);
-key = false;
-main.hide();
-INFOSCREEN.show();
-        }
-      });
-}
-*/
 //var LAT;
 //var LONG;
 
-  loadBus(LAT,LONG);
+loadBus(LAT,LONG);
 var CustomURL;
 function success(pos) {
   //LAT = String(pos.coords.latitude);
   //LONG = String(pos.coords.longitude);
   CustomURL = "http://abdulwahaab.ca/octranspo/testing.php?lat=" + LAT + "&long=" + LONG;
-  console.log(LAT);
+  /*console.log(LAT);
   console.log(LONG);
-  console.log(CustomURL);
+  console.log(CustomURL);*/
   loadBus(LAT,LONG);
   
 }
-
 function error(err) {
   if(err.code == err.PERMISSION_DENIED) {
     console.log('Location access was denied by the user.');  
+    var permission_denied = new UI.Window();
+    var error_msg = new UI.Text({
+      position: new Vector2(0, 0),
+      size: new Vector2(144, 168),
+      text: 'Location access was denined by the user.',
+      font:'DROID_SERIF_28_BOLD',
+      color:'red',
+      textOverflow:'wrap',
+      textAlign:'center',
+    	backgroundColor:'black'
+    });
+    main.hide();
+    permission_denied.add(error_msg);
+    permission_denied.show();
   } else {
     console.log('location error (' + err.code + '): ' + err.message);
   }
@@ -116,23 +90,18 @@ var options = {
   timeout: 10000
 };
 navigator.geolocation.getCurrentPosition(success, error, options);
-//var CustomURL = "http://abdulwahaab.ca/octranspo/testing.php?lat=" + LAT + "&long=" + LONG;
-//console.log(CustomURL);
 function loadBus(lat,long){
-//if(key){
   var CusURL = "http://abdulwahaab.ca/octranspo/testing.php?lat=" + lat + "&long=" + long;
 ajax(
   {
     url:CusURL,
     type:'json'
-    
   },
   function(Data){
     var Routes = Data.GetRouteSummaryForStopResult.Routes;
-    console.log(CusURL);
     for(var i = 0; i< Routes.Route.length;i++){
       BusNumbers.push(Routes.Route[i].RouteNo);
-      if (Routes.Route[i].Trips.length > 0){
+      if (Routes.Route[i].Trips && Routes.Route[i].Trips.length > 0){
         Destinations.push(Routes.Route[i].RouteHeading);
         Times.push(Routes.Route[i].Trips[0].AdjustedScheduleTime);
       }
@@ -142,14 +111,12 @@ ajax(
 
       }
     }
-    
    WRAP(); 
+    console.log("Wrap");
   }
 
 );
 }
-//}
-//}
 
 function ShowOptions(){
   var menu = new UI.Menu({
